79336c4b4fae2bd0c79a274a6587bc5061460464
RobnettSean22 <robnettsean22@gmail.com>
2020-01-17 23:05:19 +0900

    solved api get error cause by uknow middle ware proxy

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const { viewAllDevices } = require("./controllers/deviceController");
 const { viewAllStatus } = require("./controllers/statusController");
-const port = 4001;
+const port = 5000;
 
 app.use(express.json());
 
-app.get("/api/all_devices", viewAllDevices);
+app.get("/api/all_devices/", viewAllDevices);
 
 app.get("/api/all_status", viewAllStatus);
 
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      DV: null,
+      SI: null
+    };
+  }
+  componentDidMount() {
+    this.viewAllDevices();
+  }
+  viewAllDevices() {
+    axios.get("/api/all_devices").then(response => {
+      this.setState({
+        DV: response.data
+      });
+    });
+  }
+
+  render() {
+    const { DV } = this.state;
+
+    return <div>run</div>;
+  }
+}
+
+export default App;
