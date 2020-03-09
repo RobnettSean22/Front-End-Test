6c7d6ba241d984aea94a5c813f25f9b15a5350f5
RobnettSean22 <robnettsean22@gmail.com>
2020-03-09 23:50:17 +0900

    digital ocean commit

--- server/index.js
+++ server/index.js
@@ -5,10 +5,16 @@ const { viewAllStatus } = require("./controllers/statusController");
 const port = 4001;
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/../build`));
 
 app.get("/api/all_devices/", viewAllDevices);
 
 app.get("/api/all_status", viewAllStatus);
+
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`loud and clear on ${port}`);
